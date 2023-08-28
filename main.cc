0a241d442fb513e4a269d7dec335f7ca555aac93
Даниил Ермолаев <daniilermolaev@MacBook-Pro-Daniil.local>
2023-08-28 12:39:18 +0500

    OOP Injection

--- main.cc
+++ main.cc
@@ -2,106 +2,126 @@
 #include <clickhouse/client.h>
 
 using namespace drogon;
+using namespace clickhouse;
 
 typedef std::function<void(const HttpResponsePtr &)> Callback;
 
-auto create_response(Json::Value jsonBody, std::string status, std::string message, HttpStatusCode sc = HttpStatusCode::k400BadRequest) {
-    jsonBody["status"] = status;
-    jsonBody["message"] = message;
-
-    auto response = HttpResponse::newHttpJsonResponse(jsonBody);
-    if ( sc == HttpStatusCode::k400BadRequest ) { response->setStatusCode(sc); }
-    
-    return response;
-}
-
-void create_service(const HttpRequestPtr &request, Callback &&callback) {
-    Json::Value jsonBody;
-    
-    auto requestBody = request->getJsonObject();
-
-    if (requestBody == nullptr){callback(create_response(jsonBody, "error", "Not enouth data in request.")); return; }
-    if (!requestBody-> isMember("service_name")){callback(create_response(jsonBody, "error", "no `service_name` in request.")); return; }
-    if (!requestBody-> isMember("service_url")){callback(create_response(jsonBody, "error", "no `service_url` in request.")); return; }
-
-    auto service_name = requestBody->get("service_name", "guest").asString();
-    auto service_url = requestBody->get("service_url", "guest").asString();
-    LOG_INFO << "Service with data { 'service_name': '" + service_name + "', 'service_url': '" + service_url + "' } has been created!";
-    callback(create_response(jsonBody, "success", "Service has been created!"));
-    return;
-}
-
-void login_at_service(const HttpRequestPtr &request, Callback &&callback) {
-    Json::Value jsonBody;
-
-    auto requestBody = request->getJsonObject();
-
-    if (requestBody == nullptr) {callback(create_response(jsonBody, "error", "No user data")); return; }
-    if (!requestBody->isMember("username")){callback(create_response(jsonBody, "error", "no `username` in user_data.")); return; }
-    if (!requestBody->isMember("password")){callback(create_response(jsonBody, "error", "no `password` in user_data.")); return; }
-    if (!requestBody->isMember("service_public_token")){callback(create_response(jsonBody, "error", "no `service_public_token` in user_data."));}
-
-    auto username = requestBody->get("username", "guest").asString();
-    auto password = requestBody->get("password", "guest").asString();
-    auto service_public_token = requestBody->get("service_public_token", "guest").asString();
-    
-    LOG_INFO << "User with uuid has been authorized at service UUID: "<<"google.com as uuid.";
-    callback(create_response(jsonBody, "success", "SUPERTOKENFORAUTOHRIZEDEXPIREDAT12MINS"));
-    return;
-}
-
-void create_user(const HttpRequestPtr &request, Callback &&callback) {
-    Json::Value jsonBody;
-
-    auto requestBody = request->getJsonObject();
-
-    if (requestBody==nullptr){callback(create_response(jsonBody, "error", "No user data.")); return;}
-    if (!requestBody->isMember("username")){callback(create_response(jsonBody, "error", "no `username` in user data.")); return;}
-    if (!requestBody->isMember("password")){callback(create_response(jsonBody, "error", "no `password` in user data.")); return;}
-    if (!requestBody->isMember("service_public_token")){callback(create_response(jsonBody, "error", "no `service_public_token` in user data.")); return;}
-
-    auto username = requestBody->get("username", "guest").asString();
-    auto password = requestBody->get("password", "guest").asString();
-    auto service_public_token = requestBody->get("service_public_token", "guest").asString();
-
-    LOG_INFO << "User has been created for service UUID: "<<"google.com as uuid.";
-    callback(create_response(jsonBody, "success", "SUPERTOKENFORAUTOHRIZEDEXPIREDAT12MINS"));
-    return;
-
-}
-
-void service_passport(const HttpRequestPtr &request, Callback &&callback) {
-    return;
-}
-
-void user_passport(const HttpRequestPtr &request, Callback &&callback) {
-    return;
-}
-
-class AuthV3Server {
+class DatabaseConnection {
 public:
-    int run() {
+    DatabaseConnection() {
+        Client client(ClientOptions().SetHost("localhost"));
+    }
+    bool username_exists() {
+        return true;
+    }
+
+    void hash_password() {
+        return;
+    }
+    void create_user_uuid() {
+        return;
+    }
+};
+
+class AuthServer {
+public:
+    auto create_response(Json::Value jsonBody, std::string status, std::string message, HttpStatusCode sc = HttpStatusCode::k400BadRequest) {
+        jsonBody["status"] = status;
+        jsonBody["message"] = message;
+
+        auto response = HttpResponse::newHttpJsonResponse(jsonBody);
+        if ( sc == HttpStatusCode::k400BadRequest ) { response->setStatusCode(sc); }
+        
+        return response;
+    }
+    void create_service(const HttpRequestPtr &request, Callback &&callback) {
+        Json::Value jsonBody;
+        LOG_INFO << drogon::utils::getUuid();  
+        auto requestBody = request->getJsonObject();
+
+        if (requestBody == nullptr){callback(create_response(jsonBody, "error", "Not enouth data in request.")); return; }
+        if (!requestBody-> isMember("service_name")){callback(create_response(jsonBody, "error", "no `service_name` in request.")); return; }
+        if (!requestBody-> isMember("service_url")){callback(create_response(jsonBody, "error", "no `service_url` in request.")); return; }
+
+        auto service_name = requestBody->get("service_name", "guest").asString();
+        auto service_url = requestBody->get("service_url", "guest").asString();
+        LOG_INFO << "Service with data { 'service_name': '" + service_name + "', 'service_url': '" + service_url + "' } has been created!";
+        callback(create_response(jsonBody, "success", "Service has been created!"));
+        return;
+    }
+    void login_at_service(const HttpRequestPtr &request, Callback &&callback) {
+        Json::Value jsonBody;
+
+        auto requestBody = request->getJsonObject();
+
+        if (requestBody == nullptr) {callback(create_response(jsonBody, "error", "No user data")); return; }
+        if (!requestBody->isMember("username")){callback(create_response(jsonBody, "error", "no `username` in user_data.")); return; }
+        if (!requestBody->isMember("password")){callback(create_response(jsonBody, "error", "no `password` in user_data.")); return; }
+        if (!requestBody->isMember("service_public_token")){callback(create_response(jsonBody, "error", "no `service_public_token` in user_data."));}
+
+        auto username = requestBody->get("username", "guest").asString();
+        auto password = requestBody->get("password", "guest").asString();
+        auto service_public_token = requestBody->get("service_public_token", "guest").asString();
+        
+        LOG_INFO << "User with uuid has been authorized at service UUID: "<<"google.com as uuid.";
+        callback(create_response(jsonBody, "success", "SUPERTOKENFORAUTOHRIZEDEXPIREDAT12MINS"));
+        return;
+    }
+    void create_user(const HttpRequestPtr &request, Callback &&callback) {
+        Json::Value jsonBody;
+
+        auto requestBody = request->getJsonObject();
+
+        if (requestBody==nullptr){callback(create_response(jsonBody, "error", "No user data.")); return;}
+        if (!requestBody->isMember("username")){callback(create_response(jsonBody, "error", "no `username` in user data.")); return;}
+        if (!requestBody->isMember("password")){callback(create_response(jsonBody, "error", "no `password` in user data.")); return;}
+        if (!requestBody->isMember("service_public_token")){callback(create_response(jsonBody, "error", "no `service_public_token` in user data.")); return;}
+
+        auto username = requestBody->get("username", "guest").asString();
+        auto password = requestBody->get("password", "guest").asString();
+        auto service_public_token = requestBody->get("service_public_token", "guest").asString();
+
+        LOG_INFO << "User has been created for service UUID: "<<"google.com as uuid.";
+        callback(create_response(jsonBody, "success", "SUPERTOKENFORAUTOHRIZEDEXPIREDAT12MINS"));
+        return;
+    }
+
+    void service_passport(const HttpRequestPtr &request, Callback &&callback) {
+        return;
+    }
+
+    void user_passport(const HttpRequestPtr &request, Callback &&callback) {
+        return;
+    }
+    
+};
+
+
+class ServerSetup {
+public:
+    ServerSetup() {
         drogon::app().addListener("0.0.0.0",80);
 
         LOG_INFO << "Server started at 0.0.0.0:80";
 
         drogon::app()
-        .registerHandler("/create-service", &create_service, {Post});
+        .registerHandler("/create-service", &AuthServer::create_service, {Post});
         drogon::app()
-        .registerHandler("/login-in-service", &login_at_service, {Post});
+        .registerHandler("/login-in-service", &AuthServer::login_at_service, {Post});
         drogon::app()
-        .registerHandler("/create-user", &create_user, {Post});
+        .registerHandler("/create-user", &AuthServer::create_user, {Post});
 
         //Load config file
         drogon::app().loadConfigFile("../config.json");
         //Run HTTP framework,the method will block in the internal event loop
         drogon::app().run();
-        return 0;
-    }    
+    }
 };
 
+
+
 int main() {
-    AuthV3Server server;
-    server.run();
+    ServerSetup server;
+    return 0;
     
 }
